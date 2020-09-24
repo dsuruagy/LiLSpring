d63d0633aca42e591dc4270625aca1e8901970ff
Daniel Suruagy <dsuruagy@gmail.com>
2020-09-23 22:53:51 -0300

    Adding more repositories and postgres remote database access.

--- learning-spring/src/main/java/com/dsuruagy/lil/learningspring/LearningSpringApplication.java
+++ learning-spring/src/main/java/com/dsuruagy/lil/learningspring/LearningSpringApplication.java
@@ -1,6 +1,10 @@
 package com.dsuruagy.lil.learningspring;
 
+import com.dsuruagy.lil.learningspring.data.entity.Guest;
+import com.dsuruagy.lil.learningspring.data.entity.Reservation;
 import com.dsuruagy.lil.learningspring.data.entity.Room;
+import com.dsuruagy.lil.learningspring.data.repository.GuestRepository;
+import com.dsuruagy.lil.learningspring.data.repository.ReservationRepository;
 import com.dsuruagy.lil.learningspring.data.repository.RoomRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +32,34 @@ public class LearningSpringApplication {
 		public Iterable<Room> getRooms() {
 			Iterable<Room> rooms = this.roomRepository.findAll();
 			return rooms;
+		}
+	}
+
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController {
+		@Autowired
+		private GuestRepository guestRepository;
+
+		@GetMapping
+		public Iterable<Guest> getGuests() {
+			Iterable<Guest> guests = this.guestRepository.findAll();
+
+			return guests;
+		}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController {
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations() {
+			Iterable<Reservation> reservations = this.reservationRepository.findAll();
+
+			return reservations;
 		}
 	}
 }
