69289ab0da687a1f33a3b2db036a214e91029775
Daniel Suruagy <dsuruagy@gmail.com>
2020-09-29 08:39:13 -0300

    Completing the Service code.

--- learning-spring/src/main/java/com/dsuruagy/lil/learningspring/business/service/ReservationService.java
+++ learning-spring/src/main/java/com/dsuruagy/lil/learningspring/business/service/ReservationService.java
@@ -1,9 +1,12 @@
 package com.dsuruagy.lil.learningspring.business.service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.dsuruagy.lil.learningspring.business.domain.RoomReservation;
+import com.dsuruagy.lil.learningspring.data.entity.Room;
 import com.dsuruagy.lil.learningspring.data.repository.GuestRepository;
 import com.dsuruagy.lil.learningspring.data.repository.ReservationRepository;
 import com.dsuruagy.lil.learningspring.data.repository.RoomRepository;
@@ -26,6 +29,16 @@ public class ReservationService {
     }
     
     public List<RoomReservation> getReservationsForDate (LocalDate date) {
+        Iterable<Room> rooms = roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getNumber());
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+
         return null;
     }
 }
--- learning-spring/src/main/java/com/dsuruagy/lil/learningspring/data/repository/ReservationRepository.java
+++ learning-spring/src/main/java/com/dsuruagy/lil/learningspring/data/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
 package com.dsuruagy.lil.learningspring.data.repository;
 
+import java.sql.Date;
+
 import com.dsuruagy.lil.learningspring.data.entity.Reservation;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {    
+    public Iterable<Reservation> findReservationByReservationDate(Date date);
 }
