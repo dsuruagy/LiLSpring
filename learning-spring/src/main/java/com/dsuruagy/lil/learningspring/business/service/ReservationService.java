18eec3e6b138cce30937b542612808092f408be8
Daniel Suruagy <dsuruagy@gmail.com>
2020-09-30 05:55:24 -0300

    Completing the Service's code.

--- learning-spring/src/main/java/com/dsuruagy/lil/learningspring/business/service/ReservationService.java
+++ learning-spring/src/main/java/com/dsuruagy/lil/learningspring/business/service/ReservationService.java
@@ -1,11 +1,14 @@
 package com.dsuruagy.lil.learningspring.business.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.dsuruagy.lil.learningspring.business.domain.RoomReservation;
+import com.dsuruagy.lil.learningspring.data.entity.Guest;
+import com.dsuruagy.lil.learningspring.data.entity.Reservation;
 import com.dsuruagy.lil.learningspring.data.entity.Room;
 import com.dsuruagy.lil.learningspring.data.repository.GuestRepository;
 import com.dsuruagy.lil.learningspring.data.repository.ReservationRepository;
@@ -39,6 +42,22 @@ public class ReservationService {
             roomReservationMap.put(room.getId(), roomReservation);
         });
 
-        return null;
+        Iterable<Reservation> reservations = 
+            reservationRepository.findReservationByReservationDate(new java.sql.Date(date.toEpochDay()));
+        reservations.forEach(reservation -> {
+            RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
+            roomReservation.setDate(date);
+
+            Guest guest = guestRepository.findById(reservation.getGuestId()).orElse(null);
+            roomReservation.setGuestId(guest.getId());
+            roomReservation.setFirstName(guest.getFirstName());
+            roomReservation.setLastName(guest.getLastName());
+        });
+
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        for(Long id: roomReservationMap.keySet()) {
+            roomReservations.add(roomReservationMap.get(id));
+        }
+        return roomReservations;
     }
 }
