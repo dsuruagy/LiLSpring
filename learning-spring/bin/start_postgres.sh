b11cc64c152bfa1fdd427d895fad39dd1d2f3b74
Daniel Suruagy <dsuruagy@gmail.com>
2020-10-01 06:37:52 -0300

    Adding some style to pages

--- learning-spring/bin/start_postgres.sh
+++ learning-spring/bin/start_postgres.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 which psql > /dev/null || (echo "Please ensure that postgres client is in your PATH" && exit 1)
 
-mkdir -p $HOME/docker/volumes/postgres
-rm -rf $HOME/docker/volumes/postgres/data
+mkdir -p $HOME/desenv/docker/volumes/postgres
+rm -rf $HOME/desenv/docker/volumes/postgres/data
 
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql postgres
 sleep 3
