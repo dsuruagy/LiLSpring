75df2991e919f0f9ea730c7325e3051afb2a63df
Daniel Suruagy <dsuruagy@gmail.com>
2020-10-10 09:58:36 -0300

    Adding autoconfiguration module for logging aspect into the web application

--- bootEssentials/room-web-app/src/main/java/com/dsuruagy/boot/landon/roomwebapp/ApiController.java
+++ bootEssentials/room-web-app/src/main/java/com/dsuruagy/boot/landon/roomwebapp/ApiController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.frankmoley.landon.aop.Timed;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +19,7 @@ public class ApiController {
     }
 
     @GetMapping("/rooms")
+    @Timed
     public List<Room> getAllRooms() {
         return this.roomService.getAllRooms(); 
     }
