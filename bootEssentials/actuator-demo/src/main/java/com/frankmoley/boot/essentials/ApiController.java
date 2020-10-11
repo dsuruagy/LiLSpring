f0f6747c9b552ed794af99dfcc2881c3162fcc51
Daniel Suruagy <dsuruagy@gmail.com>
2020-10-11 08:06:20 -0300

    Configuring Timed and Counter to use on Metrics endpoint

--- bootEssentials/actuator-demo/src/main/java/com/frankmoley/boot/essentials/ApiController.java
+++ bootEssentials/actuator-demo/src/main/java/com/frankmoley/boot/essentials/ApiController.java
@@ -1,5 +1,8 @@
 package com.frankmoley.boot.essentials;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +15,21 @@ import java.util.List;
 public class ApiController {
 
     private final PresidentRepository presidentRepository;
+    private final Counter greetingCounter;
 
-    public ApiController(PresidentRepository presidentRepository){
+    public ApiController(PresidentRepository presidentRepository, MeterRegistry registry){
         this.presidentRepository = presidentRepository;
+        this.greetingCounter = Counter.builder("api.greeting").register(registry);
     }
 
     @GetMapping("greeting")
     public String getGreeting(){
+        greetingCounter.increment();
         return "Hello LinkedIn Learning Student";
     }
 
     @GetMapping("presidents")
+    @Timed(value = "api.getAllPresidents")
     public List<President> getAllUSPresidents(){
         List<President> presidents = new ArrayList<>();
         this.presidentRepository.findAll().forEach(presidents::add);
