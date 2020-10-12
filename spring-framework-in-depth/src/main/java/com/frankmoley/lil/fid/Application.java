0ebe1ebae07fec64eca303b65f90f77bfcf49d50
Daniel Suruagy <dsuruagy@gmail.com>
2020-10-12 07:57:13 -0300

    Using AnnotationConfigApplicationContext to instatiate beans

--- spring-framework-in-depth/src/main/java/com/frankmoley/lil/fid/Application.java
+++ spring-framework-in-depth/src/main/java/com/frankmoley/lil/fid/Application.java
@@ -1,15 +1,18 @@
 package com.frankmoley.lil.fid;
 
+import com.frankmoley.lil.fid.config.ApplicationConfig;
 import com.frankmoley.lil.fid.service.GreetingService;
 import com.frankmoley.lil.fid.service.OutputService;
 import com.frankmoley.lil.fid.service.TimeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
-        TimeService timeService = new TimeService(true);
-        OutputService outputService = new OutputService(greetingService, timeService);
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        OutputService outputService = context.getBean(OutputService.class);
 
         for (int i=0;i<5;i++){
             outputService.generateOutput("Frank");
