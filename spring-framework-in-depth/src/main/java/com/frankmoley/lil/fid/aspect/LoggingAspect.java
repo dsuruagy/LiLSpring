2168754f917a91e342695426a6aa473f50740d2e
Daniel Suruagy <dsuruagy@gmail.com>
2020-10-19 06:22:31 -0300

    Using an AfterReturning advice that logs the return value of a method.

--- spring-framework-in-depth/src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ spring-framework-in-depth/src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.frankmoley.lil.fid.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 @Aspect
@@ -17,12 +19,12 @@ public class LoggingAspect {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("@annotation(Loggable)")
-    public void executeLogging() {
+    @Pointcut("@annotation(Loggable)") // the pointcut expression
+    public void executeLogging() { // the pointcut signature, used by advice annotations for reference
 
     }
 
-    @Before("executeLogging()")
+    @Before("executeLogging()") // an advice annotation, referring to the pointcut signature
     public void logMethodCall(JoinPoint joinPoint) {
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
@@ -33,6 +35,19 @@ public class LoggingAspect {
             Arrays.asList(args).forEach(arg ->
                     message.append(arg).append(" | "));
             message.append("]");
+        }
+        LOGGER.info(message.toString());
+    }
+
+    // an advice annotation, referring to the pointcut signature
+    @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    public void logMethodReturning(JoinPoint joinPoint, Object returnValue) {
+        StringBuilder message = new StringBuilder();
+        if(returnValue instanceof Collection) {
+            message.append("Returning: ")
+                    .append(((Collection) returnValue).size()).append(" instances.");
+        } else {
+            message.append("Returning: ").append(returnValue.toString());
         }
         LOGGER.info(message.toString());
     }
