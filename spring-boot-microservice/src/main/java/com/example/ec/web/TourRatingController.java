c6db8d6d608e0f5e2ba7e3d2dbbc1ada160abaa7
Daniel Radi Suruagy Monteiro <dsuruagy@gmail.com>
2020-10-23 07:28:36 -0300

    Using RESTful API to create a Tour Rating.

--- spring-boot-microservice/src/main/java/com/example/ec/web/TourRatingController.java
+++ spring-boot-microservice/src/main/java/com/example/ec/web/TourRatingController.java
@@ -1,10 +1,17 @@
 package com.example.ec.web;
 
+import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
 import com.example.ec.repo.TourRatingRepository;
 import com.example.ec.repo.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.NoSuchElementException;
 
 /**
  * Tour Rating Controller
@@ -27,5 +34,29 @@ public class TourRatingController {
 
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Validated
+    public TourRating createTourRating(@RequestBody @Valid RatingDto dto,
+                                       @PathVariable(value="tourId") Integer tourId) {
+        Tour tour = tourRepository.findById(tourId)
+                .orElseThrow(() -> new NoSuchElementException("Tour not find with id " + tourId));
 
+        TourRatingPk tourRatingPk = new TourRatingPk(tour, dto.getCustomerId());
+        TourRating tourRating = new TourRating(tourRatingPk, dto.getScore(), dto.getComment());
+
+        return tourRatingRepository.save(tourRating);
+    }
+
+    /**
+     * Exception handler if NoSuchElementException is thrown
+     *
+     * @param ex exception
+     * @return Error message String
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
+    }
 }
