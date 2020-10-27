fae55c64a02ff9f1f56ceabd38a842d7e5015dbe
Daniel Radi Suruagy Monteiro <dsuruagy@gmail.com>
2020-10-26 21:51:45 -0300

    Define the schema and default data from SQL files. Removing JSON import.

--- spring-boot-microservice/src/main/java/com/example/ec/domain/Tour.java
+++ spring-boot-microservice/src/main/java/com/example/ec/domain/Tour.java
@@ -1,6 +1,7 @@
 package com.example.ec.domain;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
 /**
@@ -9,9 +10,9 @@ import java.util.Objects;
  * Created by Mary Ellen Bowman
  */
 @Entity
-public class Tour {
+public class Tour implements Serializable{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column
@@ -37,14 +38,14 @@ public class Tour {
 
 
     @ManyToOne
+    @JoinColumn(name="tour_package_code")
     private TourPackage tourPackage;
 
     @Column
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
 
     @Column
-    @Enumerated
     private Region region;
 
     public Tour(String title, String description, String blurb, Integer price, String duration, String bullets,
@@ -72,48 +73,24 @@ public class Tour {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getBlurb() {
         return blurb;
     }
 
-    public void setBlurb(String blurb) {
-        this.blurb = blurb;
-    }
-
     public Integer getPrice() {
         return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
     }
 
     public String getDuration() {
         return duration;
     }
 
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
     public String getBullets() {
         return bullets;
-    }
-
-    public void setBullets(String bullets) {
-        this.bullets = bullets;
     }
 
     public String getKeywords() {
@@ -124,28 +101,12 @@ public class Tour {
         return tourPackage;
     }
 
-    public void setTourPackage(TourPackage tourPackage) {
-        this.tourPackage = tourPackage;
-    }
-
-    public void setKeywords(String keywords) {
-        this.keywords = keywords;
-    }
-
     public Difficulty getDifficulty() {
         return difficulty;
     }
 
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
     public Region getRegion() {
         return region;
-    }
-
-    public void setRegion(Region region) {
-        this.region = region;
     }
 
     @Override
