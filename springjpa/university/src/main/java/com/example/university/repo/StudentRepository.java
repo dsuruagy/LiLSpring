1a071fe06d1872ee3383697cb68b54aaba62e888
Daniel Radi Suruagy Monteiro <dsuruagy@gmail.com>
2020-12-14 06:28:35 -0300

    Query method clauses and expressions

--- springjpa/university/src/main/java/com/example/university/repo/StudentRepository.java
+++ springjpa/university/src/main/java/com/example/university/repo/StudentRepository.java
@@ -4,6 +4,8 @@ import com.example.university.domain.Person;
 import com.example.university.domain.Student;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 
     // Simple Query Methods
@@ -12,13 +14,23 @@ public interface StudentRepository extends CrudRepository<Student, Integer> {
     Iterable<Student> findByAttendeeLastName(String lastName);
 
     // Query Methods with Clauses and Expressions
+    // https://docs.spring.io/spring-data/jpa/docs/1.8.x/reference/html/#repository-query-keywords
+
+    // Conditional Expression:
     Student findByAttendeeFirstNameAndAttendeeLastName(String jane, String doe);
-    String findByAttendee(Person person);
+    Student findByAttendee(Person person);
+    // Expressions with Operators:
     Iterable<Student> findByAgeGreaterThan(int age);
     Iterable<Student> findByAgeLessThan(int age);
+    Iterable<Student> findByFullTimeOrAgeLessThan(boolean fullTime, int maxAge);
     Iterable<Student> findByAttendeeLastNameIgnoreCase(String lastName);
-    Iterable<Student> findByAttendeeLastNameLike(String lastName);
-    String findFirstByOrderByAttendeeLastNameAsc();
-    String findTopByOrderByAgeDesc();
-    String findTop3ByOrderByAgeDesc();
+    Iterable<Student> findByAttendeeLastNameLike(String lastName); // Wildcard search
+
+    // Expression limiting and Ordering
+    // Finds highest student in the alphabet
+    Student findFirstByOrderByAttendeeLastNameAsc();
+    //Find the oldest student
+    Student findTopByOrderByAgeDesc();
+    // Find 3 oldest students
+    List<Student> findTop3ByOrderByAgeDesc();
 }
