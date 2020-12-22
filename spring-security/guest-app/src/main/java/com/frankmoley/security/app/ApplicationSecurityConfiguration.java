8e117437a8c54deec39b3dac2754660626c5123d
Daniel Radi Suruagy Monteiro <dsuruagy@gmail.com>
2020-12-22 06:36:46 -0300

    Implementing basic authentication.

---
+++ spring-security/guest-app/src/main/java/com/frankmoley/security/app/ApplicationSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.frankmoley.security.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // CSRF - Cross-site request forgery
+        http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+    }
+}
